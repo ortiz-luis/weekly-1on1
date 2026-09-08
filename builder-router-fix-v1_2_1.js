994622598dbb8d4cadf632262b1c02a58a13c6c7
l.ortiz <luis.ortiz@pasqal.io>
2026-09-08 16:49:08 +0200

    deploy: refresh public Builder runtime

--- builder-router-fix-v1_2_1.js
+++
@@ -1,8 +0,0 @@
-(()=>{
-  const isBuilder=()=>location.hash.replace(/^#/,'').split('/')[0]==='builder';
-  window.addEventListener('hashchange',()=>{
-    if(!isBuilder()) return;
-    queueMicrotask(()=>render());
-  });
-  if(isBuilder()) queueMicrotask(()=>render());
-})();
